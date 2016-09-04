e34d26c14da8e4d7777e34f3c950749603b9de9a
James <07009003@hope.ac.uk>
2016-09-04 23:02:07 +0100

    cors

--- src/main/java/uk/co/jamesowen/application/MainApplication.java
+++ src/main/java/uk/co/jamesowen/application/MainApplication.java
@@ -9,6 +9,7 @@ import io.dropwizard.auth.AuthFactory;
 import io.dropwizard.auth.basic.BasicAuthFactory;
 import io.dropwizard.setup.Environment;
 import org.apache.log4j.Logger;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.mongodb.morphia.Morphia;
 import uk.co.jamesowen.configuration.BlogPostServiceConfiguration;
 import uk.co.jamesowen.configuration.MongoConfiguration;
@@ -17,7 +18,10 @@ import uk.co.jamesowen.resource.MailController;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.Properties;
 
 import static com.mongodb.MongoCredential.createCredential;
@@ -33,6 +37,16 @@ public class MainApplication extends Application<BlogPostServiceConfiguration> {
 
     public void run(BlogPostServiceConfiguration configuration, Environment environment) {
 
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 environment.jersey().register(MailController.class);
 
     }
